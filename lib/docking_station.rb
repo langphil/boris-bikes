67f18584e513bafa1af2d2f554835c3c12c08159
Phil Lang <plangphoto@gmail.com>
2017-07-04 19:13:02 +0100

    refactoring and cleanup

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,12 +2,13 @@ require_relative 'bike'
 
 class DockingStation
   attr_reader :bike
+  
   def release_bike
     fail "bike not present" unless @bike
     @bike
   end
 
-  def dock (bike)
+  def dock(bike)
     @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,8 @@
 require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike}
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to(:bike)}
 
   describe '#release_bike' do
     it "doesn't give out a bike if there's none" do
@@ -13,10 +15,6 @@ describe DockingStation do
       expect(subject.release_bike).to eq bike
     end
   end
-
-  it { is_expected.to respond_to(:dock).with(1).argument }
-
-  it { is_expected.to respond_to(:bike)}
 
   it 'docks a bike' do
     bike = Bike.new
